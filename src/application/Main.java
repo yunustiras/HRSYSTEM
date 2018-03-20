56454b3769aff48a5f5e24fe9439a719a9e5f694
Emre-Kul <emrekul35@gmail.com>
2018-03-20 22:53:45 +0300

    Example page created with ScreenBuilder

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,38 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("HRSystem");
+		initHomeLayout();
+	}
+	
+	public void initHomeLayout(){
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,600,600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//load home layout
+	        FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/HomePage.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
