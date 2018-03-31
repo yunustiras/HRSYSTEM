368f47e3f46e373efd1f2de9e9b3cf7220662323
pleycpl <h4rvey@protonmail.com>
2018-03-31 12:27:38 +0100

    updated login module

--- src/main/java/hrsystem/modules/login/controller/LoginView.java
+++ src/main/java/hrsystem/modules/login/controller/LoginView.java
@@ -1,11 +1,30 @@
 package hrsystem.modules.login.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginView implements Initializable {
+    @FXML
+    private TextField textEmail;
+
+    @FXML
+    private PasswordField textPassword;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        String email = textEmail.getText();
+        String password = textPassword.getText();
+
+        System.out.println(String.format("Email: %s, Password: %s", email, password));
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
