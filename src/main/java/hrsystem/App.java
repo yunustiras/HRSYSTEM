a4dcb225922e14415afb0ce4b21b3873abad41b0
pleycpl <h4rvey@protonmail.com>
2018-04-29 21:51:15 +0100

    using sqlitejdbc and confirmbox

--- src/main/java/hrsystem/App.java
+++ src/main/java/hrsystem/App.java
@@ -1,19 +1,34 @@
 package hrsystem;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import hrsystem.util.SQLiteJDBC;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import hrsystem.util.StageController;
+import hrsystem.util.ConfirmBox;
+
 public class App extends Application {
     public static String FxmlScenePath = "/hrsystem/fxml/login.fxml";
     public static void main(String[] args) {
+        try {
+            SQLiteJDBC.isHaveDriver();
+        } catch (Exception e) {
+            System.out.println("You dont have sqlite jdbc driver");
+        }
         launch(args);
     }
 
     @Override
 	public void start(Stage primaryStage) {
+        primaryStage.setOnCloseRequest(e -> {
+            e.consume();
+            Boolean answer = ConfirmBox.display("Exit", "Are you want to exit?");
+            if (answer == true) {
+                primaryStage.close();
+            }
+        });
         try {
             StageController.switchStage(primaryStage, App.FxmlScenePath);
         }
@@ -22,3 +37,4 @@ public class App extends Application {
         }
 	}
 }
+
